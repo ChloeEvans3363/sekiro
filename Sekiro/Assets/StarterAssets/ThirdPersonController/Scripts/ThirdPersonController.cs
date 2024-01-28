ffd123502528d6e2c4821e57921bd1450caf9166
Chloe <cre3363@g.rit.edu>
2024-01-28 16:02:57 -0500

    Player can't move when attacking

--- Sekiro/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Sekiro/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -265,14 +265,16 @@ namespace StarterAssets
                     RotationSmoothTime);
 
                 // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                if (gameObject.GetComponent<Player>().state != PlayerState.attacking)
+                    transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
 
 
             Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
             // move the player
-            _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
+            if(gameObject.GetComponent<Player>().state != PlayerState.attacking)
+                _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
                              new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
             // update animator if using character
